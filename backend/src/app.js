1a6de46e04f249780e9a6f6c29b6d7ebc99191e6
fieldmarcel <shiva9590t@gmail.com>
2025-09-26 22:18:44 +0530

    minor cors change

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,7 +11,23 @@ const app = express();
 app.use(helmet());
 app.use(express.json());
 app.use(morgan('dev'));
-app.use(cors({ origin: 'http://localhost:5174', credentials: true })); // adjust origin
+const allowedOrigins = [
+  'http://localhost:5174',                  // local dev
+  'https://colbin-assignment-6.onrender.com' // deployed frontend
+];
+
+app.use(cors({
+  origin: function(origin, callback) {
+    // allowing  requests with no origin (like Postman)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  credentials: true
+}));
 
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
